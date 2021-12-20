ee3fc9b0d962906743e6d9da7160e2d6aa33a4d6
Agustin <agustin@craftech.io>
2021-12-20 17:25:25 -0300

    fix: allow failure on vault command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pipeline-generator',
-    version='0.8.5',
+    version='0.8.6',
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['templates/*.jinja2']},
